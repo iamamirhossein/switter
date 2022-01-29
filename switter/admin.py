03c5a1d6572501ac9269733101699f96818c81b6
imamirhossein <amirhosseinkhoshdel@gmail.com>
2022-01-29 22:21:14 +0330

    refactor: :art: removed registered

--- switter/admin.py
+++ switter/admin.py
@@ -1,13 +1,2 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
 
-
-# Register your models here.
-
-class UserAdmin(admin.ModelAdmin):
-    model = User
-    fields = ['username', 'email']
-
-
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
