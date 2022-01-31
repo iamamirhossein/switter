fbdfc459ee383af355516f7188ee9597aedda123
imamirhossein <amirhosseinkhoshdel@gmail.com>
2022-01-31 19:38:02 +0330

    feat: :fire: added ProfileListView to account app

--- account/views.py
+++ account/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Profile
 
-# Create your views here.
+
+class ProfileListView(ListView):
+    model = Profile
+    paginate_by = 10
+    template_name = 'switter/profiles.html'
+
+    def get_queryset(self):
+        return Profile.objects.filter(is_private=False)
