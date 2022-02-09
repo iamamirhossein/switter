ed0f6514627a27fce0c203f9c362a7dc1695ecb6
imamirhossein <amirhosseinkhoshdel@gmail.com>
2022-02-09 23:32:46 +0330

    feat: :art: Create model Sweet

--- switter/models.py
+++ switter/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Sweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sweets')
+    body = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    draft = models.BooleanField(default=False)
