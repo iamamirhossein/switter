616691abdad1aa1b1e4735b0297758e8e27a2e10
imamirhossein <amirhosseinkhoshdel@gmail.com>
2022-02-13 18:53:22 +0330

    feat: :art: added SweetManager to model

--- switter/models.py
+++ switter/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class SweetManager(models.Manager):
+
+    def published(self):
+        return self.filter(draft=False)
+
+
 class Sweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sweets')
     body = models.CharField(max_length=250)
@@ -9,12 +15,18 @@ class Sweet(models.Model):
     draft = models.BooleanField(default=False)
     re_sweet = models.BooleanField(default=True)
 
+    class Meta:
+        verbose_name = 'sweet'
+        verbose_name_plural = 'sweets'
+
     def __str__(self):
         return self.body[:45]
 
     @property
     def faves_count(self):
         return self.faves.count()
+
+    objects = SweetManager()
 
 
 class Fave(models.Model):
