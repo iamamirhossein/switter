addc15bf8ac219e9a1370e8e153950e4663ad17a
imamirhossein <amirhosseinkhoshdel@gmail.com>
2022-02-21 23:03:31 +0330

    feat: :zap: added  some attrs to form

--- switter/forms.py
+++ switter/forms.py
@@ -3,7 +3,16 @@ from .models import Sweet
 
 
 class SweetForm(forms.ModelForm):
-    body = forms.CharField(required=True)
+    body = forms.CharField(
+        required=True,
+        widget=forms.widgets.Textarea(
+            attrs={
+                "placeholder": "New Sweet...",
+                "class": "textarea is-success is-medium",
+            }
+        ),
+        label="",
+    )
     draft = forms.CheckboxInput()
     re_sweet = forms.CheckboxInput()
 
