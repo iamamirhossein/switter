1e1407b4c23e1cd0233102de7627ca139e18af90
imamirhossein <amirhosseinkhoshdel@gmail.com>
2022-02-28 23:22:29 +0330

    feat: :art: added drafts object to SweetManager

--- switter/models.py
+++ switter/models.py
@@ -7,6 +7,9 @@ class SweetManager(models.Manager):
     def published(self):
         return self.filter(draft=False).order_by('-created_at')
 
+    def drafts(self):
+        return self.filter(draft=True).order_by('-created_at')
+
 
 class Sweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sweets')
