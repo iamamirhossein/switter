97d28993feb1a83485106a0cfbb0749e5a6b0117
imamirhossein <amirhosseinkhoshdel@gmail.com>
2022-03-01 20:16:57 +0330

    feat: :zap: added sweet_detail to urls

--- switter/urls.py
+++ switter/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('sweet/drafts', views.drafts, name="drafts"),
     path('sweet/create/', views.SweetCreateView.as_view(), name='sweet_create'),
-    path('sweet/delete/<int:pk>', views.SweetDeleteView.as_view(), name='sweet_delete'),
+    path('sweet/<int:pk>/delete', views.SweetDeleteView.as_view(), name='sweet_delete'),
+    path('sweet/<int:pk>', views.SweetDetailView.as_view(), name='sweet_detail'),
 ]
